37938c80c10f1aa208eccbb15c85761c7842c9fe
morteza_tlz <talezadeh.m@gmail.com>
2023-08-27 10:21:50 -0400

    implemented simple get price quote

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", (req,res)=>{
-    const result= "";
+
     res.render("index.ejs");
+});
+
+app.post("/get-price", async(req,res)=>{
+    try{
+        console.log(req.body.sybmol);
+        const result= await axios.get("https://api.blockchain.com/v3/exchange/tickers/" + req.body.sybmol);
+        res.render("index.ejs",{priceQuote:JSON.stringify(result.data.last_trade_price)} );
+    } catch(err){
+        console.log(err.message);
+    }
+
+
 });
 
 
